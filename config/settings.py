550e22ad8630d0cc4112eacf4c108ae2ecc78d21
Kirill <lartsev200@yandex.ru>
2024-02-20 12:26:02 +0300

    fix: .idea

--- config/settings.py
+++ config/settings.py
@@ -91,10 +91,6 @@ DATABASES = {
         'HOST': env.str('DB_HOST', 'localhost'),
         'PORT': env.int('DB_PORT', 5432),
     },
-    'extra': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
 }
 
 ###########################
