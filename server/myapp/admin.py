e48e33e875742fedadc837813b612fa1c242e803
Kirill <lartsev200@yandex.ru>
2024-02-20 12:17:05 +0300

    update: architecture, app, db

--- server/myapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import News, Users, Feedback
-
-admin.site.register(News)
-admin.site.register(Users)
-admin.site.register(Feedback)
--- server/myapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import NewsView, UsersView, FeedbackView
-
-urlpatterns = [
-    path('news/', NewsView().get, name='news'),
-    path('users/', UsersView().get, name='users'),
-    path('feedback/', FeedbackView().get, name='feedback'),
-]
