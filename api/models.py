3829b182a7721448cf036dc0974513a335c2accc
Kirill <lartsev200@yandex.ru>
2024-05-02 14:49:15 +0300

    fix: time

--- api/models.py
+++ api/models.py
@@ -5,11 +5,12 @@ class News(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=255)
     description = models.TextField()
-    news_date = models.DateField()
+    news_date = models.DateTimeField()
     address = models.TextField()
     url = models.TextField()
     img = models.TextField()
-    priority = models.IntegerField()
+    priority = models.IntegerField(default=1)
+
     def __str__(self):
         return self.title
 
